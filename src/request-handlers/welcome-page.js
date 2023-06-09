8cc51303f2af9cf52b7277bebe856f850dc3c0e7
Travis Butcher <tbutcher@esri.com>
2023-06-09 17:57:01 +0000

    update welcome pages

--- src/request-handlers/welcome-page.js
+++ src/request-handlers/welcome-page.js
@@ -1,5 +1,19 @@
 function handleRequest (req, res) {
-  res.status(200).send('Welcome to Koop!')
+  // Define your list data
+    // Get the host variable from the request
+    const host = req.headers.host;
+  const items = ['malariacases/rest/services/qrur9Dvnyt5/oZg33kd9taw/FeatureServer/0', 'dhis2/rest/services/qrur9Dvnyt5/oZg33kd9taw/FeatureServer/0', 'facilities/rest/services/qrur9Dvnyt5/oZg33kd9taw/FeatureServer/0'];
+
+  let response = '<html><head><title>DHIS Connections</title></head><body>';
+  response += '<h1>Services</h1>';
+  response += '<ul>';
+  items.forEach((item) => {
+    response += `<li><a href='https://${host}/${item}'>${host}/${item}</a></li>`;
+  });
+  response += '</ul>';
+  response += '</body></html>';
+  
+  res.status(200).send(response)
 }
 
 module.exports = handleRequest
