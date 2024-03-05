a9a3672dc53623a7e1532c55fc5a7ef97c435e6e
Travis Butcher <tbutcher@esri.com>
2024-03-05 09:13:31 -0800

    update koop models and cleanup alias

--- providers/malariacases/src/parseGeoJson.js
+++ providers/malariacases/src/parseGeoJson.js
@@ -12,27 +12,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-const finalFields = ["OBJECTID", "gender", "age", "ouname","oucode"];
 
 module.exports = (input, fieldInfo) => {
   return {
     type: "FeatureCollection",
     features: 
     input.rows.map((row, i) => {
-      console.log("Row", row);
-      return {
-            type: "Feature",
-            properties: {
-              OBJECTID: i + 1,
-              admin: row[11],
-              casedate: row[2],
-              gender: row[18],
-              age: parseInt(row[19]),
-            },
-            geometry: JSON.parse(row[8]),
-          };
-      /*
-      console.log("Row", fieldInfo);
       const fieldProperties = row.reduce((acc, value, i) => {
         if (
           fieldInfo.geometryColumnIndex &&
@@ -41,16 +26,16 @@ module.exports = (input, fieldInfo) => {
           return acc;
         }
         const fieldConfig = fieldInfo.basePropsConfig[i];
-        console.log("Field Config", fieldConfig);
         if (fieldConfig.type === "Integer") {
           value = parseInt(value);
+        } else if (fieldConfig.type === "Double") {
+          value = parseFloat(value);
         }
-
         if (fieldConfig.type === "String") {
           value = value ? value.toString() : '';
         }
 
-        acc[fieldInfo[i].name] = value;
+        acc[fieldInfo.basePropsConfig[i].name] = value;
         return acc;
       }, {});
       
@@ -61,19 +46,7 @@ module.exports = (input, fieldInfo) => {
           ...fieldProperties
         },
         geometry: JSON.parse(row[fieldInfo.geometryColumnIndex]),
-      }; */
-      //   return {
-      //     type: "Feature",
-      //     properties: {
-      //       id: i + 1,
-      //       admin: row[11],
-      //       casedate: row[2],
-      //       gender: row[17],
-      //       status: row[13],
-      //       age: parseInt(row[18]),
-      //     },
-      //     geometry: JSON.parse(row[8]),
-      //   };
+      };
     }),
   };
 };
